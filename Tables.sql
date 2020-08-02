07def2c8e5f670947b21df3d4b068b94c16b32ac
Edmund <chen_mingyie@yahoo.com>
2020-08-02 16:14:34 +0800

    Setup project to run on Edmund's machine

---
+++ Tables.sql
@@ -0,0 +1,19 @@
+create table users (id int not null AUTO_INCREMENT, user_name varchar(50) not null, user_email varchar(50) not null, user_password varchar(50) not null, 
+age int not null, country varchar(20) not null, time_to_read int, old_news_interest varchar(50), local_news_interest varchar(50), popular_tweets_interest varchar(50), PRIMARY KEY (id));
+
+select * from users;
+
+create table topic_preferences(id int not null AUTO_INCREMENT, user_id int not null, topic_type varchar(50) not null, topic_name varchar(50) not null, interest_level varchar(50) not null, PRIMARY KEY(id), FOREIGN KEY (user_id) REFERENCES users(id));
+
+select * from topic_preferences;
+
+create table news_source_preferences(id int not null AUTO_INCREMENT, user_id int not null, source_name varchar(50) not null, interest_level varchar(50) not null, PRIMARY KEY(id), FOREIGN KEY (user_id) REFERENCES users(id));
+
+select * from news_source_preferences;
+
+SET SQL_SAFE_UPDATES=0;
+delete from news_source_preferences;
+
+delete from topic_preferences;
+
+delete from users;
--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:gopalg0pal@localhost/news_curator'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/news_curator'
 db = SQLAlchemy(app)
 
 from app import routes
